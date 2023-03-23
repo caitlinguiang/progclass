684f08b1fe01e5a5dbec37f8d9204f01f77fdb15
caitlinguiang <128510003+caitlinguiang@users.noreply.github.com>
2023-03-22 21:44:26 -0400

    Add files via upload

---
+++ encode.py
@@ -0,0 +1,29 @@
+def encoder(password):      # caitlin guiang
+    list(password)       # convert string into list
+    new_password = ''        # create new string for encoded password
+    for element in password:
+        element = int(element)
+        if element < 7:         # encodes by adding 3 to each element
+            element += 3
+        elif element >= 7:       # gets rid of double-digit numbers, only uses 0-9
+            element -= 7
+        new_password += str(element)
+    return new_password
+
+def print_menu():        # creates menu
+    print("Menu")
+    print("-" * 13)
+    print("1. Encode\n2. Decode\n3. Quit\n")
+
+if __name__ == "__main__":
+    while True:
+        print_menu()
+        option = int(input("Please enter an option: "))
+        if option == 1:      # gets user input for password and encodes it
+            password = input("Please enter your password to encode: ")
+            new_password = encoder(password)
+            print("Your password has been encoded and stored!\n")
+        elif option == 2:        # displays encoded password and original password
+            print(f"The encoded password is {new_password}, and the original password is {password}.\n")
+        elif option == 3:        # ends program
+            break
